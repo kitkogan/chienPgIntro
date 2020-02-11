bc52c326d7610f7fafc9eadd5216bf44069a786b
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2020-02-11 16:38:16 -0600

    db connection

--- server/server.js
+++ server/server.js
@@ -2,13 +2,20 @@
 const express = require( 'express' );
 const app = express();
 const bodyParser = require( 'body-parser' );
+const pg = require( 'pg' );
 // uses
 app.use( express.static( 'server/public' ) );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 // globals
 const port = 5000;
 // db setup
-
+const pool = new pg.Pool({
+    database: 'music_library',
+    host: 'localhost',
+    port: 5432,
+    max: 12,
+    idleTimeoutMillis: 30000
+}); //end pool
 // server up
 app.listen( port, ()=>{
     console.log( 'server up on:', port );
